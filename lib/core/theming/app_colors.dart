497cbffc5ceb30c611310541a2e7144df8041139
ngure1 <ngugijoseph700@gmail.com>
2024-09-25 19:37:26 +0300

    style:gem: : add app colors

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color secondaryColor = const Color(0xFF8A8781);
+  static Color primaryInverseColor = const Color(0xFFC5A54E);
+  static Color surfaceColor = const Color(0xFFFFF8F1);
+  static Color outlineColor = const Color(0xFF655E50);
+  static Color primaryContainer = const Color(0xFF534000);
+  static Color onPrimaryContainer = const Color(0xFFFFFFFF);
+  static Color surfaceContainer = const Color(0xFFEAE1D4);
+  static Color onSurfaceContainer = const Color(0xFF484325);
+}
